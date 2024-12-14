a9119b1afbf96e131ffa68fefe2e48db4eb407ef
Dharani <ponnidharani@gmail.com>
2024-12-14 21:16:10 +0530

    add gemini integration

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ app.get("/", (req, res) => {
     res.render("index.ejs");
   });
 
+app.post("/submit", async (req, res) => {
+  const submission = req.body;
+  const formArray = Object.entries(submission);
+  const prompt = `Write an email requesting leave. The details are mentioned. Write 'sir/mam' as recipient name. dont use any []. don't make up details. write as a parent. dont assume gender of student. format date & time to human friendly.  ${formArray.toString()}`
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
